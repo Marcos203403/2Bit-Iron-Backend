6309bf7146ba97a571f05b7dc0bd4b5faa05fe07
Marcos203403 <a29004@svalero.com>
2026-06-11 00:55:41 +0200

    chore: setup jest testing environment and fix port blocking

--- src/index.js
+++ src/index.js
@@ -21,8 +21,10 @@ app.get('/', (req, res) => {
     res.json({ status: 'ok', message: 'BitIron API running' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
